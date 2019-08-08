6140d3bb63b30fc50b11276691d2a97a091304cd
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-08 18:24:33 +0100

    created outline of methods

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,42 @@
+
 class Animal
+    @@all = []
+
+    attr_reader :nickname, :species
+    attr_accessor :weight
+
+    def initialize(nickname:, species:, weight:)
+        @nickname = nickname
+        @species = species
+        @weight = weight
+        @@all << self
+    end
+
+    def nickname
+        @nickname
+    end
+
+    def species
+        @species
+    end
+
+    def weight
+        @weight
+    end
+
+    def all
+        #array of all animal instances
+        @@all
+    end
+
+    def zoo
+        #return ZOO instance that this instance belongs to
+        Zoo.all.select {|zoo| }
+    end
+
+    def find_by_species(species)
+        #array of all the animals of this species
+    end
+
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,47 @@
+
 class Zoo
+    @@all = []
+
+    attr_accessor :name, :location
+
+    def initialize(name:, location:)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def location
+        #location of the zoo instance
+        @location
+    end
+
+    def name
+        #name of the zoo instance
+        @name 
+    end
+
+    def all
+        #array of the zoo instances
+        @@all
+    end
+
+    def animal_species(species)
+        #array of all the animals in this instance of zoo, with that species
+        #select?
+    end
+
+    def find_by_species(species)
+        #array of all the animals in this instance of zoo, with that species
+        # .find
+    end
+
+    def animal_nicknames
+        #return array of all the nicknames of all animals in this instance fo zoo
+    end
+
+    def find_by_location(location)
+        #return array of all the zoos in that location
+    end
+
 
 end
