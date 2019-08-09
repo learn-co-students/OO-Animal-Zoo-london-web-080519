6d812e7bb0017ead958bf3e41e9335b2cfc3beb5
github username <github email address>
2019-08-09 12:56:15 +0100

    Done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,32 @@
+require "pry"
+
 class Animal
 
+    attr_accessor :zoo, :species 
+    attr_reader :nickname, :weight 
+
+    @@all = [] 
+
+    def initialize(species, weight, nickname, zoo)
+        @species = species 
+        @weight = weight 
+        @nickname = nickname 
+        @zoo = zoo 
+        @@all << self 
+        
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.find_by_species(species)
+        animal_species = []
+        self.all.select do | animal | 
+            if animal.species == species 
+                animal_species << animal.species
+            end 
+        end 
+    end 
+    
 end
