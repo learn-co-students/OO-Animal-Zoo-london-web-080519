04bfbeab286d48cd1201c7e04fd09982bab2c18d
github username <github email address>
2019-08-09 14:22:54 +0100

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,21 @@
 class Animal
+    attr_reader :name, :nickname, :species
+    attr_accessor :weight
+    @@all = []
+    def initialize(name, species, weight, nickname)
+        @name = name # ?? return first word ??
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all  << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.select {|animal| animal.species == species}
+    end
 
 end
